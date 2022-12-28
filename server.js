e1823403b2dc9767f31fcbb72406b7c2189a1b04
IMadeThisJustToPostThis <matzielbiz@gmail.com>
2022-12-28 16:42:53 -0500

    added activity

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ const routes = require('./controllers');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const cwd = process.cwd();
+const activity = cwd.includes('01-Activities')
+  ? cwd.split('/01-Activities/')[1]
+  : cwd;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
