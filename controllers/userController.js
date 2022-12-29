dd4910eb08da05a275cdb03f80d2a321f7b37831
IMadeThisJustToPostThis <matzielbiz@gmail.com>
2022-12-28 19:28:49 -0500

    added controller framework

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,27 @@
 const { Thought, User } = require('../models');
 
 const userController = {
+    getUsers(req, res) {
 
+    },
+    getUser(req, res) {
+
+    },
+    createUser(req, res) {
+
+    },
+    updateUser(req, res) {
+
+    },
+    deleteUser(req, res) {
+
+    },
+    addFriend(req, res) {
+
+    },
+    removeFriend(req, res) {
+
+    }
 };
 
 module.exports = userController;
