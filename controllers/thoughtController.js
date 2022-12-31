3386ad561f8a21a6cf253b066c04aff55dd250e5
IMadeThisJustToPostThis <matzielbiz@gmail.com>
2022-12-31 15:57:44 -0500

    added thought controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -12,32 +12,108 @@ const thoughtController = {
             });
     },
     getThought(req, res) {
-        Thought.findOne({ _id: req.params.userId }).select('-__v')
-        .then((thoughtData) => {
-            if(!thoughtData) {
-                return res.status(404).json({ message: 'Thought does not exist.' });
-            }
-            res.status(200).json(thoughtData);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).json(err);
-        });
+        Thought.findOne({ _id: req.params.thoughtId }).select('-__v')
+            .then((thoughtData) => {
+                if (!thoughtData) {
+                    return res.status(404).json({ message: 'Thought does not exist.' });
+                }
+                res.status(200).json(thoughtData);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).json(err);
+            });
     },
     createThought(req, res) {
-
+        Thought.create(req.body)
+            .then((thoughtData) => {
+                return User.findOneAndUpdate(
+                    { _id: req.body.userId },
+                    { $push: { thoughts: thoughtData._id } },
+                    { new: true }
+                );
+            })
+            .then((userData) => {
+                if (!userData) {
+                    return res.status(404).json({ message: 'User does not exist.' });
+                }
+                res.status(200).json(userData);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).json(err);
+            });
     },
     updateThought(req, res) {
-
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { new: true })
+            .then((thoughtData) => {
+                if (!thoughtData) {
+                    return res.status(404).json({ message: 'Thought does not exist.' });
+                }
+                res.status(200).json(thoughtData);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).json(err);
+            });
     },
     deleteThought(req, res) {
-
+        Thought.findOneAndRemove({ _id: req.params.thoughtId })
+            .then((thoughtData) => {
+                if (!thoughtData) {
+                    return res.status(404).json({ message: 'Thought does not exist.' });
+                }
+                return User.findOneAndUpdate(
+                    { thoughts: req.params.thoughtId },
+                    { $pull: { thoughts: req.params.thoughtId } },
+                    { new: true }
+                );
+            })
+            .then((userData) => {
+                if (!userData) {
+                    return res.status(404).json({ message: 'User does not exist.' });
+                }
+                res.status(200).json(userData);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).json(err);
+            });
     },
     addReaction(req, res) {
-
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { new: true })
+            .then((thoughtData) => {
+                if (!thoughtData) {
+                    return res.status(404).json({ message: 'Thought does not exist.' });
+                }
+                res.status(200).json(thoughtData);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).json(err);
+            });
     },
     removeReaction(req, res) {
-
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { new: true })
+            .then((thoughtData) => {
+                if (!thoughtData) {
+                    return res.status(404).json({ message: 'Thought does not exist.' });
+                }
+                res.status(200).json(thoughtData);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).json(err);
+            });
     }
 };
 
