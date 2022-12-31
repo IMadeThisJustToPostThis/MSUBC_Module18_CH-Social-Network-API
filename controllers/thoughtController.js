bf9b91f3c641795d470575e6c57b1ee752f76173
IMadeThisJustToPostThis <matzielbiz@gmail.com>
2022-12-31 14:20:51 -0500

    added get routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,10 +2,27 @@ const { Thought, User } = require('../models');
 
 const thoughtController = {
     getThoughts(req, res) {
-
+        Thought.find().select('-__v')
+            .then((thoughtData) => {
+                res.status(200).json(thoughtData);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).json(err);
+            });
     },
     getThought(req, res) {
-
+        Thought.findOne({ _id: req.params.userId }).select('-__v')
+        .then((thoughtData) => {
+            if(!thoughtData) {
+                return res.status(404).json({ message: 'Thought does not exist.' });
+            }
+            res.status(200).json(thoughtData);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).json(err);
+        });
     },
     createThought(req, res) {
 
@@ -20,7 +37,7 @@ const thoughtController = {
 
     },
     removeReaction(req, res) {
-        
+
     }
 };
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,10 +2,28 @@ const { Thought, User } = require('../models');
 
 const userController = {
     getUsers(req, res) {
-
+        User.find().select('-__v')
+            .then((userData) => {
+                res.status(200).json(userData);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).json(err);
+            });
     },
     getUser(req, res) {
-
+        User.findOne({ _id: req.params.userId }).select('-__v')
+        .populate('friends').populate('thoughts')
+        .then((userData) => {
+            if(!userData) {
+                return res.status(404).json({ message: 'User does not exist.' });
+            }
+            res.status(200).json(userData);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).json(err);
+        });
     },
     createUser(req, res) {
 
